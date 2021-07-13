787b3e3c280c1feca5a5cafb6ef8f3e9d3696624
Yevhenii <freshjs.fte@gmail.com>
2021-07-13 18:43:01 +0300

    Add sum function

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 
 /* 1 */
 const num = 2;
-const str = 'str';
+const str = '3';
 
-const sum = str + num;
-console.log(sum); // обьединение строк
+const getSumOfTwo = function (num1, num2) {
+    // checks
+    if (isNaN(num1) || isNaN(num2)) {
+        return false;
+    }
+
+    const sum = parseFloat(num1) + parseFloat(num2);
+    
+    return sum;
+}
+
+const result = getSumOfTwo(num, str);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 2 */
 const bool1 = true;
 const bool2 = true;
 
 let operation = bool1 + bool2;
-console.log(operation)
+// console.log(operation)
 
 operation = bool2 - bool1;
-console.log(operation)
+// console.log(operation)
 
 /* 3 */
 const string = '22.6';
 const num2 = 4.3;
 
 const sum2 = Number(string) + num2;
-console.log(sum2);
+// console.log(sum2);
