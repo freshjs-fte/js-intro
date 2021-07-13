8ee41bfcac8673dcd3cce4b9f9a7bbc2fd59dd5f
Yevhenii <freshjs.fte@gmail.com>
2021-07-13 19:07:13 +0300

    Fix func for boolean

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 /* 1 */
 const num = 2;
 const str = '3';
+const sec = num;
 
 const getSumOfTwo = function (num1, num2) {
     // checks
@@ -9,7 +10,7 @@ const getSumOfTwo = function (num1, num2) {
         return false;
     }
 
-    const sum = parseFloat(num1) + parseFloat(num2);
+    const sum = Number(num1) + Number(num2);
     
     return sum;
 }
