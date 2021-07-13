c56e2a098c92daf44a3eb3a8dfdd656370b9fb7d
Yevhenii <freshjs.fte@gmail.com>
2021-07-13 20:51:12 +0300

    Add tasks solution

--- index.js
+++ index.js
@@ -3,20 +3,36 @@
 const num = 2;
 const str = '3';
 
-function getSumOfTwo (num1, num2) {
+const getSumOfTwo = function (num1, num2) {
     // checks
     if (isNaN(num1) || isNaN(num2)) {
         return false;
     }
-
-    const sum = Number(num1) + Number(num2);
     
-    return sum;
-}
+    return (Number(num1) + Number(num2));
+};
 
 const result = getSumOfTwo(num, str);
 
 console.log(result);
+
+/*  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,7 +45,67 @@ console.log(result);
     2. Функцию которая возвращает минимальное число из двух
     3. Функция которая определяет является ли число четным
 
-    (4.*) Функция которая находит корень квадратного уравнения 
+    (4.*) Функция которая возвращает корень квадратного уравнения 
     (метод дискриминанта)
 
 */
+
+/* 1 */
+const getMaxOfTwo = function (num1, num2) {
+
+    if(isNaN(num1) || isNaN(num2)) {
+        return false;
+    }
+
+    if(parseFloat(num1) > parseFloat(num2)) {
+        return num1;
+    }
+    
+    return num2;
+};
+
+let res = getMaxOfTwo('35', '30');
+console.log(res)
+
+
+/* 2 */
+const getMinOfTwo = function (num1, num2) {
+
+    if(isNaN(num1) || isNaN(num2)) {
+        return false;
+    }
+
+    if(parseFloat(num2) > parseFloat(num1)) {
+        return num1;
+    }
+    
+    return num2;
+};
+
+res = getMinOfTwo('35', '30');
+console.log(res)
+
+/* 3 */
+const isEven = function (number) {
+    if(isNaN(number)) {
+        return null;
+    }
+
+    return number % 2 === 0;
+}
+
+console.log(isEven(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
